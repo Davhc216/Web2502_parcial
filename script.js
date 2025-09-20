ec1d322958c0c8f6ff8889274e7322dca7858601
David Hoyos <carlosdavidhc2611@gmail.com>
2025-09-20 10:43:10 -0500

    Se agrega la constante para la petición de información del país que se desee consultar en la pagina web, se realiza la prueba de consulta a la API revisando el inspector web de la página y efectivamente trae la información del país consultado

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 const obtenerValorInput = () => {
     let inputTexto = document.getElementById("input_pais");
     let valor = inputTexto.value;
-    console.log(valor);
+    peticionAPI(valor);
+}
+
+const peticionAPI = (pais) => {
+    const baseurl = 'https://restcountries.com/v3.1/';
+    const endpoint = `name/${pais}`;
+    const url = `${baseurl}${endpoint}`;
+
+    axios.get(url)
+    .then(respuesta => console.log(respuesta.data))
+    .catch(error => console.log(error));
 }
